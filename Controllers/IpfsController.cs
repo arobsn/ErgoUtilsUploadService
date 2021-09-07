acaeea7e1caaabf908672a717e40273f1c4bd6a1
anon-br <sigmanon@protonmail.com>
2021-09-07 20:59:48 -0300

    Add /check/ endpoint

--- Controllers/IpfsController.cs
+++ Controllers/IpfsController.cs
@@ -23,18 +23,42 @@ namespace ErgoUtilsUploadService.Controllers
         public async Task<IActionResult> Upload(IFormFile file)
         {
             var client = new RestClient("https://api.nft.storage");
-
             using var stream = file.OpenReadStream();
             using var ms = new MemoryStream();
             stream.CopyTo(ms);
-
             var request = new RestRequest("upload", Method.POST);
             request.AddHeader("Authorization", $"Bearer {_config["ApiKey"]}");
             request.AddParameter(file.Name, ms.ToArray(), ParameterType.RequestBody);
 
             var response = await client.ExecuteAsync(request);
+            return ResponseFrom(response);
+        }
 
-            return Ok(JsonSerializer.Deserialize(response.Content, typeof(object)));
+        [HttpGet("check/{cid}")]
+        public async Task<IActionResult> Check([FromRoute] string cid)
+        {
+            var client = new RestClient("https://api.nft.storage");
+            var request = new RestRequest(Method.GET);
+            request.AddUrlSegment("check", cid);
+            request.AddHeader("Authorization", $"Bearer {_config["ApiKey"]}");
+
+            var response = await client.ExecuteAsync(request);
+            return ResponseFrom(response);
+        }
+
+        private ObjectResult ResponseFrom(IRestResponse response)
+        {
+            return StatusCode((int)response.StatusCode, ParseResult(response));
+        }
+
+        private static object ParseResult(IRestResponse response)
+        {
+            if (string.IsNullOrWhiteSpace(response.Content))
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize(response.Content, typeof(object));
         }
     }
 }
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ An upload service used by [ErgoUtils](https://github.com/anon-real/ErgoUtils) to
 ## API Endpoints
 |Endpoint|Method|Desciption|
 |---|:---:|---|
-|`/ipfs`|`POST`|Used to upload a file|
+|`/ipfs`|`POST`|Store a file|
+|`/ipfs/check/{cid}`|`GET`|Check if a CID of an NFT is being stored by nft.storage.
 |`/version`|`GET`|Check service's commit hash|
